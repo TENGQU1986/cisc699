f94d06949de5b0a8a41575d60e4a17f605f684b1
Patrick Qu <quteng.bo@gmail.com>
2021-06-13 18:56:08 -0400

    implement login page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import React, {useState} from 'react'
-import Child from './Child'
-import './App.css'
-// import Perf from 'react-addons-perf'
-
-// window.Perf = Perf
+import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db, provider } from "./firebase/firebase";
+import "./App.css";
+import Login from "./Login";
+import Parent from "./Parent";
 
 const App = () => {
-    const [counter, setCounter] = useState(0)
-    const [input, setInput] = useState('')
-    const list = [1, 2, 3]
+  const [user] = useAuthState(auth);
+  if (!user) return <Login />;
+  return <Parent user={user} />;
+};
 
-    const handleCounterClick = () => {
-        setCounter(counter + 1)
-    }
-
-    const onInputChange = (e) => {
-        const value = e.target.value
-        setInput(value)
-    }
-
-    return (
-        // <div className="App">
-        //     <h1>Hello from Parent: {counter}</h1>
-        //     <button onClick={handleCounterClick}>+1</button>
-        //     <input type="text" onChange={onInputChange}/>
-        //     <Child counter={counter} list={list}/>
-        // </div>
-        <h1>Hello World!!</h1>
-    )
-}
-
-export default App
+export default App;
---
+++ src/Parent.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Child from "./Child";
+import "./App.css";
+
+const Parent = () => {
+  const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState("");
+  const list = [1, 2, 3];
+
+  const handleCounterClick = () => {
+    setCounter(counter + 1);
+  };
+
+  const onInputChange = (e) => {
+    const value = e.target.value;
+    setInput(value);
+  };
+  return (
+    <div className="App">
+      <h1>Hello from Parent: {counter}</h1>
+      <button onClick={handleCounterClick}>+1</button>
+      <input type="text" onChange={onInputChange} />
+      <Child counter={counter} list={list} />
+    </div>
+  );
+};
+
+export default Parent;
